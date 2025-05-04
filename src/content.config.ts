57f7816aa8696186f0bfa0d8f8419d4487470e23
Terje Lafton <terje@lafton.io>
2025-05-04 14:48:39 +0200

    feat: devlog support

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
 import { glob } from "astro/loaders";
 import { defineCollection, z } from "astro:content";
+import { date } from "astro:schema";
 
 const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
@@ -12,4 +13,13 @@ const blog = defineCollection({
   }),
 });
 
-export const collections = { blog };
+const devlog = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/devlog" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.string().date(),
+    tags: z.array(z.string()).default([]),
+  }),
+});
+
+export const collections = { blog, devlog };
